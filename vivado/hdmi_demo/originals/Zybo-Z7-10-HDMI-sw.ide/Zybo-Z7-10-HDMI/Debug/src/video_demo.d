b34a74b5b980060304210301854957be9e81d326
Petru Brasoveanu <petrubvunitbv@gmail.com>
2026-01-23 13:16:27 +0200

    simplificat codul, 2 puncte

---
+++ vivado/hdmi_demo/originals/Zybo-Z7-10-HDMI-sw.ide/Zybo-Z7-10-HDMI/Debug/src/video_demo.d
@@ -0,0 +1,86 @@
+src/video_demo.o src/video_demo.o: \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/video_demo.c \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/video_demo.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/video_capture/video_capture.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_hw.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_i.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xdebug.h \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/video_capture/xvtc.h \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/video_capture/xvtc_hw.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpio.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpio_l.h \
+ d:\zybo-z7-sw\src\zybo-z7-10-hdmi\src\intc\intc.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xscugic.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xscugic_hw.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_exception.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_spinlock.h \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/display_ctrl/display_ctrl.h \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/display_ctrl/vga_modes.h \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/display_ctrl/xvtc.h \
+ d:\zybo-z7-sw\src\zybo-z7-10-hdmi\src\dynclk\dynclk.h \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/intc/intc.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xuartps.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xuartps_hw.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xplatform_info.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_util.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h \
+ D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/timer_ps/timer_ps.h \
+ D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h
+D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/video_demo.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/video_capture/video_capture.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_hw.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_i.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xdebug.h:
+D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/video_capture/xvtc.h:
+D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/video_capture/xvtc_hw.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpio.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpio_l.h:
+d:\zybo-z7-sw\src\zybo-z7-10-hdmi\src\intc\intc.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xscugic.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xscugic_hw.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_exception.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_spinlock.h:
+D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/display_ctrl/display_ctrl.h:
+D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/display_ctrl/vga_modes.h:
+D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/display_ctrl/xvtc.h:
+d:\zybo-z7-sw\src\zybo-z7-10-hdmi\src\dynclk\dynclk.h:
+D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/intc/intc.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xuartps.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xuartps_hw.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xplatform_info.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_util.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h:
+D:/Zybo-Z7-SW/src/Zybo-Z7-10-HDMI/src/timer_ps/timer_ps.h:
+D:/Zybo-Z7-SW/ws/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
